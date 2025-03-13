b25138a8729b8ea20bc5e0d05d5b3303dd5c28b2
AndrewPechnikov <73440973+AndrewPechnikov@users.noreply.github.com>
2025-03-13 11:00:18 +0000

    init

--- main.py
+++ main.py
@@ -5,20 +5,17 @@ from background import keep_alive
 from handlers import router
 
 
-
-
-
-
-
 async def main():
-    bot = Bot(token = '7597075440:AAFGbuJH5W8-t02xRsdWWbLxhwLS5Kh0grU')
+    bot = Bot(token='7597075440:AAFGbuJH5W8-t02xRsdWWbLxhwLS5Kh0grU')
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
+
+
+keep_alive()
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
         print('Бот вимкнений!')
-
